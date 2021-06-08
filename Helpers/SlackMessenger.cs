0a89a35bccf62020224335190cec1e4c96c405ee
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2021-06-08 20:14:53 +0530

    send the slack messages

---
+++ Helpers/SlackMessenger.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using webapp2.Models;
+
+namespace webapp2.Helpers
+{
+    public class SlackMessenger
+    {
+        private readonly Uri  _uri;
+        private readonly Encoding _encoding = new UTF8Encoding ();
+        
+        private void PostMessage (SlackMessage model)
+        {
+            var payload = new SlackPayload
+            {
+                Channel = "system-dev",
+                Text = model.ToString(),
+            };
+
+            var payloadjson = JsonConvert.SerializeObject(payload);
+            using (var client = new WebClient())
+            {
+                var data = new NameValueCollection { ["payload"] = payloadjson };
+                var response = client.UploadValues(_uri, "POST", data);
+                //return _encoding.GetString(response);
+            }
+        }
+    }
+}
---
+++ Models/SlackPayload.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapp2.Models
+{
+    public class SlackPayload
+    {
+        [JsonProperty("Channel")]
+        public string Channel { get; set; }
+        [JsonProperty("Text")]
+        public string Text { get; set; }
+    }
+}
