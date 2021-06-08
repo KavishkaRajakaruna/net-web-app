ab1e8b1ff316b7cb088030d24b16608c5d181701
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2021-06-08 21:02:27 +0530

    S3 file uploading part started.

---
+++ Models/S3FileUpload.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapp2.Models
+{
+    public class S3FileUpload
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Type { get; set; }
+        [Required]
+        public int Level { get; set; }
+        [Required]
+        [Display(Name="File")]
+        public IFormFile FormFile { get; set; }
+    }
+}
